c3626baf16d8a9e9758c7e5253005c8e0eb4187d
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-06 11:48:27 -0300

    Aula 03 ATV 02.py

--- Aula 03 ATV 02.py
+++ Aula 03 ATV 02.py
@@ -3,11 +3,13 @@
 # muito alta ou muito baixa.
 import random
 num = random.randint(1,20)
-
+qtdTentativas = 1
 tentativa = int(input("Digite um número de 1 a 20 para acertar o número secreto: "))
 while tentativa != num:
     if num > tentativa:
         tentativa = int(input("O número que você chutou está menor que o secreto. Tente de novo: "))
     elif num < tentativa:
         tentativa = int(input("O número que você chutou está maior que o secreto. Tente de novo: "))
+    qtdTentativas += 1
 print("Parabéns! Você acertou!")
+print(f"Você teve {qtdTentativas} tentativas.")
