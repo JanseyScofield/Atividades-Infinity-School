fbb40e82026ea9a9d1bd769e30fad6ca78d7390b
Aluno da Infinity School <aluno@3D.local>
2024-04-13 11:46:33 -0300

    Treino para subir com Git

---
+++ aula4.py
@@ -0,0 +1,30 @@
+# Faça um programa, utilizando while, que permita o usuário fazer
+# contas de adição enquanto quiser.
+# Exercício de revisão
+
+# soma = 0
+
+# num = float(input("Digite um número: "))
+# escolha = ''
+# while escolha != 'N':
+#     soma+= num
+#     escolha = input("Deseja adicionar outro número? 'S' para Sim e 'N' para Não: ").upper()
+#     match(escolha):
+#         case 'N':
+#             break
+#         case 'S':
+#             num = float(input("Digite outro número: "))
+# print(f"A soma de todos os números é igual a {soma}.")
+
+
+# for letra in "Adan Melo Scofield da Silva ":
+#     if letra in 'aeiou' or letra in 'AEIOU':
+#         print(letra)
+
+soma = 0 
+
+for i in range(5):
+    num = float(input("Digite um número: "))
+    soma += num
+
+print(soma)
