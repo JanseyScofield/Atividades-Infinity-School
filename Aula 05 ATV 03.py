96d001063e41552c2838ab8226a8f58d3a1287aa
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2024-04-20 12:58:15 -0300

    Aula 05 ATV 03.py

--- Aula 05 ATV 03.py
+++ Aula 05 ATV 03.py
@@ -1,8 +1,8 @@
 # Faça um programa que peça 5 números e mostre o maior entre eles.
 
-numMaior = 0
+numMaior = float()
 for i in range(0,5):
-    valor = (int(input("Digite um número: ")))
+    valor = (float(input("Digite um número: ")))
     if valor >= numMaior:
         numMaior = valor
 print(f"O maior número é: {numMaior}")
